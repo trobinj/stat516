c0247c0420712f77c42f86a5b3cae9ec06caecfc
Timothy R. Johnson <trobinj@gmail.com>
2024-04-30 13:50:24 -0700

    lecture 5/1

---
+++ wildebeest.R
@@ -0,0 +1,8 @@
+wildebeest <-
+structure(list(marrow = structure(c(2L, 1L, 3L, 2L, 1L, 3L, 2L, 
+1L, 3L, 2L, 1L, 3L), levels = c("OG", "SWF", "TG"), class = "factor"), 
+    sex = structure(c(1L, 1L, 1L, 2L, 2L, 2L, 1L, 1L, 1L, 2L, 
+    2L, 2L), class = "factor", levels = c("F", "M")), predation = structure(c(2L, 
+    2L, 2L, 2L, 2L, 2L, 1L, 1L, 1L, 1L, 1L, 1L), levels = c("N", 
+    "P"), class = "factor"), count = c(26, 32, 8, 14, 43, 10, 
+    6, 26, 16, 7, 12, 26)), row.names = c(NA, -12L), class = "data.frame")
